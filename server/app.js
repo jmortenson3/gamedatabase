5f663669e17d21da5f236a646351beaf39c8a5fd
Josiah Mortenson <josiah.mortenson@gmail.com>
2018-07-06 23:17:05 -0500

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+// get specific game
+// GET /game/:id
+
+// get games based on query string
+// GET /game/{querystring}
+
+// get a list of game genres
+// GET /genres
+
+// add a game to the database
+// POST /games
+app.get('/', (req, res, next) => {
+  res.send(`oh shit whaddup`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Node server running on port ${PORT}`);
+});
